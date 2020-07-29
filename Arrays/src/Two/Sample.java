fa3ea49cb8e07d6a1b9bdeb01a7ec29267f11c37
LAPTOP-J2A6VURN\rajin <neelimayadav127@gmail.com>
2020-07-29 18:53:41 -0500

    Arrays completed

---
+++ Arrays/src/Two/Sample.java
@@ -0,0 +1,18 @@
+package Two;
+
+public class Sample
+{
+ private int r[]= {4,5,9};
+ private String name[]= {"Archana","Mahesh","Neelima","Naresh"};
+ private int y[]= {6,10,9};
+ private String valve[]= {"akhil","chotu","doly"};
+ private float u[]= {65f,90f,12f,35f};
+ private double n[]= {0.8d,123d,97412d};
+ private float k[]= {73f,21f,89f};
+ private double h[]= {7891d,978612d,0.54d,0.123d};
+ private char rate[]= {'6','y','5','q'};
+ private boolean v[]= {true,false,true};
+ private char z[]= {'3','i','z','p'};
+ private boolean x[]= {false,true,false,false,false};
+ 
+}
