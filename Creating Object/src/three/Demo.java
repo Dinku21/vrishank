ec0ce39a066b071ec53c85ca50ac0fd8bc52cef9
LAPTOP-J2A6VURN\rajin <neelimayadav127@gmail.com>
2020-07-29 18:55:14 -0500

    Creating Object completed

---
+++ Creating Object/src/three/Demo.java
@@ -0,0 +1,15 @@
+package three;
+
+public class Demo
+{
+
+	public static void main(String[] args)
+	{
+	  Sample s=new Sample();
+	  
+	  System.out.println(s.q);
+      System.out.println(s.name);
+      System.out.println(s.valve[0]);
+	}
+
+}
---
+++ Creating Object/src/three/Sample.java
@@ -0,0 +1,8 @@
+package three;
+
+public class Sample 
+{
+  public int q=7;
+  public String name="mahesh";
+  public String valve[]= {"big","small"};
+}
